00947f2bc81f51666b24e7ac279dbe574670390c
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2018-02-10 16:39:34 +0000

    Kick, stun and run
    Something

--- NinjaPirates/Assets/Scripts/PlayerController.cs
+++ NinjaPirates/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-
+	/// <summary>
+	/// This is the object that holds all the options the players share.
+	/// </summary>
+	public PlayerOptions po;
 	public Collider2D leftKickCollider, rightKickController;
 	public Collider2D bodyCollider;
 
@@ -12,22 +15,55 @@ public class PlayerController : MonoBehaviour {
 
 	public PlayerController opponent;
 	public Vector2 velocity;
-	public Collider2D groundCollider;
-
-	public int airJumps;
+	
 	int currentAirJumps;
 	public Rigidbody2D rb;
-	public float runSpeed, jumpHeight;
-
+	
 	public KeyCode left, right, action;
+
+	
+	public bool stunned, canKick;
 
 	// Use this for initialization
 	void Start () {
-		currentAirJumps = airJumps;
+		currentAirJumps = po.airJumps;
+		stunned = false;
 	}
 	
-	public void kickEvent(){
+	/// <summary>
+	/// This gets called once the other player manages to kick you.
+	/// </summary>
+	/// <param name="opponentPosition">Vector2 other players position</param>
+	public void kickEvent(Vector2 opponentPosition){
 		
+		Vector2 kickDirection = (Vector2)this.transform.position - opponentPosition;
+
+		rb.AddForce(kickDirection * po.kickForce);
+
+		stunned = true;
+		StartCoroutine("Stunned");
+	
+	}
+
+	/// <summary>
+	/// Start cooldown until you are not stunned anymore.
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator Stunned() {
+    
+		yield return new WaitForSeconds(po.stunDuration);
+
+		stunned = false;
+	}
+
+	/// <summary>
+	/// Start the cooldown to be able to kick again.
+	/// </summary>
+	/// <returns></returns>
+	IEnumerator KickCoolDown(){
+		yield return new WaitForSeconds(po.kickCoolDown);
+
+		canKick = true;
 	}
 
 	// Update is called once per frame
@@ -35,50 +71,64 @@ public class PlayerController : MonoBehaviour {
 		
 		velocity = rb.velocity;
 
-		if(Input.GetKeyDown(action)){
-			if(bodyCollider.IsTouching(groundCollider)){
+
+		// inputs
+		if(!stunned){
+
+			// the action key is the kick/jump key. 	
+			if(Input.GetKeyDown(action)){
+
+
+				//prioritize kicks over jumping.
+				if(leftKickCollider.IsTouching(OpponentBodyCollider) || rightKickController.IsTouching(OpponentBodyCollider)){
+					
+					//TODO: decide that if you cant kick, you should jump instead.
+					if(canKick){
+						canKick = false;
+						StartCoroutine("KickCoolDown");
+						opponent.SendMessage("kickEvent", (Vector2)this.transform.position);
+					}
 				
-				currentAirJumps = airJumps;
-				rb.AddForce(new Vector2(0, jumpHeight));
-
-			}  else if (currentAirJumps > 0){
-				currentAirJumps --;
-				rb.AddForce(new Vector2(0, jumpHeight));
-			}else {
-
-				if(Input.GetKey(left)){
-					//check if leftKickCollider is touching the other players body collider
-					if(leftKickCollider.IsTouching(OpponentBodyCollider)){
-						opponent.SendMessage("kickEvent");
-					}
 				}
-				if(Input.GetKey(right)){
-					//check if leftKickCollider is touching the other players body collider
-					if(rightKickController.IsTouching(OpponentBodyCollider)){
-						opponent.SendMessage("kickEvent");
-					}
+				// the player can't kick. check if he can jump instead.
+				else {
+
+					if(bodyCollider.IsTouching(po.groundCollider)){
+						//reset airJumps to max extra jumps.
+						currentAirJumps = po.airJumps;
+						//jump!
+						rb.AddForce(new Vector2(0, po.jumpHeight));
+
+					}  
+					//check if the player still has airJumps left.
+					else if (currentAirJumps > 0){
+						//spend air jump
+						currentAirJumps --;
+						//jump!
+						rb.AddForce(new Vector2(0, po.jumpHeight));
+					} 
 				}
-
 			}
+
+			//
+			if(Input.GetKey(left)){
+				if(this.velocity.x < po.runSpeed){
+					rb.AddForce(new Vector2(-po.runSpeed, 0));
+				}
+			} 
+
+			if(Input.GetKey(right)){	
+				if(this.velocity.x < po.runSpeed){
+					rb.AddForce(new Vector2(po.runSpeed, 0));
+				}
+			} 
+			// stop the player from running.
+			//if(Input.GetKeyUp(right) || Input.GetKey(left)){
+			//	rb.velocity = new Vector2(0, rb.velocity.y);
+			//}
+
+
+
 		}
-
-		if(Input.GetKey(left)){
-			if(this.velocity.x < runSpeed){
-				rb.AddForce(new Vector2(-runSpeed, 0));
-			}
-		} 
-
-		if(Input.GetKey(right)){	
-			if(this.velocity.x < runSpeed){
-				rb.AddForce(new Vector2(runSpeed, 0));
-			}
-		} 
-		
-		if(Input.GetKeyUp(right) || Input.GetKey(left)){
-			rb.velocity.Set(0, rb.velocity.y);
-		}
-
-
-
 	}
 }
---
+++ NinjaPirates/Assets/Scripts/PlayerOptions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOptions : MonoBehaviour {
+
+	public Collider2D groundCollider;
+
+	/// <summary>
+	/// how hard a player should kick.
+	/// </summary>
+	public float kickForce;
+	/// <summary>
+	/// how long in seconds the player should be stunned if kicked.
+	/// </summary>
+	public float stunDuration;
+	/// <summary>
+	/// how many 'double-jumps' a player should have.
+	/// </summary>
+	public int airJumps;
+	/// <summary>
+	/// How fast a player should run.
+	/// </summary>
+	public float runSpeed;
+	/// <summary>
+   	/// how high a player should jump.
+   	/// </summary>
+	public float jumpHeight;
+	/// <summary>
+	/// how long until a player should wait in between kicks.
+	/// </summary>
+	public float kickCoolDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
