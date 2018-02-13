1524c50f7aa79a8f0b8d35547ed98b1b7b40b22c
bjornt16 <bjornt16@ru.is>
2018-02-13 17:37:21 +0000

    commented audioManager and tweaked it a bit

--- NinjaPirates/Assets/Scripts/AudioManager.cs
+++ NinjaPirates/Assets/Scripts/AudioManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine.Audio;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
@@ -22,15 +23,23 @@ public class AudioManager : MonoBehaviour
     }
 
     public bool playOnStart;
-    public Audio[] audioClip;
-    public Audio[] musicClip;
+
+    //lists to hold the audio/music clips
+    public List<Audio> audioClip;
+    public List<Audio> musicClip;
+
+    //reference to current music playing (used to stop it)
     Audio musicPlaying;
+    //reference to musicPlaying's position in the musicClip array
     private int musicPlayingIndex;
+
+    //bools to set if the musicList or current music should loop;
     public bool loopMusicList;
     public bool loopCurrentMusic;
 
     private void Start()
     {
+        //assign each audio/music a source (not best practice but should be fine for this game)
         foreach (Audio a in audioClip)
         {
             a.Init(gameObject.AddComponent<AudioSource>());
@@ -46,36 +55,45 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    Coroutine music = null;
+    //reference to the playMusic coroutine function (used to stop it)
+    Coroutine playMusicCoroutine = null;
+
+    //a coroutine function that plays from the musicClip array.
     public IEnumerator PlayMusic(int index = 0)
     {
-        if(!(index > musicClip.Length-1))
+        //if index not outofbounds it's safe to run
+        if(!(index > musicClip.Count-1 || index < 0))
         {
             musicPlayingIndex = index;
             do
             {
                 musicPlaying = musicClip[musicPlayingIndex];
                 musicPlaying.Source.Play();
+                //wait til the end of the clip
                 yield return new WaitForSeconds(musicPlaying.clip.length);
+
+                //if not looping current music, get next musicClip index
                 if (!loopCurrentMusic)
                 {
-                    musicPlayingIndex = musicPlayingIndex == musicClip.Length - 1 ? 0 : musicPlayingIndex + 1;
+                    musicPlayingIndex = musicPlayingIndex == musicClip.Count - 1 ? 0 : musicPlayingIndex + 1;
                 }
             }
             while (loopMusicList);
         }
     }
 
+    //a function to stop the music playing, and the looping coroutine
     public void StopMusic()
     {
         musicPlaying.Source.Stop();
-        StopCoroutine(music);
-        music = null;
+        StopCoroutine(playMusicCoroutine);
+        playMusicCoroutine = null;
     }
 
+    //a function to toggle the music on and off.
     public void ToggleMusic()
     {
-        if (music != null)
+        if (playMusicCoroutine != null)
         {
             Debug.Log("stop");
             StopMusic();
@@ -83,13 +101,14 @@ public class AudioManager : MonoBehaviour
         else
         {
             Debug.Log("start");
-            music = StartCoroutine(PlayMusic());
+            playMusicCoroutine = StartCoroutine(PlayMusic(musicPlayingIndex));
         }
     }
 
+    //a function to get an audio clip by it's name.
     private Audio AudioClipByName(string name)
     {
-        Audio a = Array.Find(audioClip, sound => sound.name == name);
+        Audio a = Array.Find(audioClip.ToArray(), sound => sound.name == name);
         if (a == null)
         {
             Debug.Log("Can't find sound with name: " + name);
@@ -97,9 +116,10 @@ public class AudioManager : MonoBehaviour
         return a;
     }
 
+    //a function to get a music clip by it's name.
     private Audio MusicClipByName(string name)
     {
-        Audio a = Array.Find(musicClip, sound => sound.name == name);
+        Audio a = Array.Find(musicClip.ToArray(), sound => sound.name == name);
         if (a == null)
         {
             Debug.Log("Can't find music with name: " + name);
@@ -107,15 +127,20 @@ public class AudioManager : MonoBehaviour
         return a;
     }
 
+    //function that starts playing a clip by name
     public void PlayAudioClip(string name)
     {
         Audio a = AudioClipByName(name);
         a.Source.Play();
     }
+
+    //function that starts playing music by name
     public void PlayMusicClip(string name)
     {
         Audio m = MusicClipByName(name);
-        m.Source.Play();
+        musicPlayingIndex = musicClip.IndexOf(m);
+        StopMusic();
+        ToggleMusic();
     }
 
 }
